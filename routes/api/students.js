ee3685b208c42756bf9726706854f3459753378e
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-19 19:12:13 +0500

    apply express-validation on Student

---
+++ routes/api/students.js
@@ -0,0 +1,29 @@
+import express from "express";
+const router = express.Router();
+import { check, validationResult } from "express-validator/check";
+// route POST api/students
+// desc Register Student
+// access public
+router.post(
+  "/",
+  [
+    check("name", "Name is Required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password of minimum 6 characters"
+    ).isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
+module.exports = router;

79ba9ad3c87aaff195c4d39ffb9d10f9114c39dc
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-07-24 04:09:44 +0500

    Get token of the registered student with jwt

--- routes/api/students.js
+++ routes/api/students.js
@@ -1,6 +1,11 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
-import { check, validationResult } from "express-validator/check";
+const { check, validationResult } = require("express-validator");
+const Student = require("../../models/Student");
+const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 // route POST api/students
 // desc Register Student
 // access public
@@ -16,14 +21,86 @@ router.post(
       "Please enter a password of minimum 6 characters"
     ).isLength({
       min: 6
-    })
+    }),
+    check("qualification", "Qualification is Required")
+      .not()
+      .isEmpty(),
+    check("address", "Address is Required")
+      .not()
+      .isEmpty(),
+    check("skills", "skills is Required")
+      .not()
+      .isEmpty(),
+    check("age", "Age is Required")
+      .not()
+      .isEmpty(),
+    check("mobile", "Mobile is Required")
+      .not()
+      .isEmpty()
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User Route");
+    const {
+      name,
+      email,
+      password,
+      qualification,
+      address,
+      skills,
+      age,
+      mobile
+    } = req.body;
+    try {
+      // See if student exists
+      let student = await Student.findOne({ email });
+      if (student) {
+        res.status(400).json({ errors: [{ msg: "Student already Exists" }] });
+      }
+      // Get students gravtar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      student = new Student({
+        name,
+        email,
+        password,
+        avatar,
+        qualification,
+        skills,
+        address,
+        age,
+        mobile
+      });
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      student.password = await bcrypt.hash(password, salt);
+
+      await student.save();
+
+      // Return jsonwebtoken
+      const payload = {
+        student: {
+          id: student.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 module.exports = router;

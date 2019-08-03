1378e26f69dae90b4cf6491821b3a46f5fb1ccab
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-08-03 16:37:52 +0500

    modity model of admin

--- models/Admin.js
+++ models/Admin.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 const AdminSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true

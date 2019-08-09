32b302d5568ad5f01658c667f3e4f084c31285af
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-08-09 12:53:36 +0500

    Student can apply on job and view companies

--- routes/api/students.js
+++ routes/api/students.js
@@ -126,15 +126,6 @@ router.put(
   [
     auth,
     [
-      check("title", "Title is Required")
-        .not()
-        .isEmpty(),
-      check("company", "Company is Required")
-        .not()
-        .isEmpty(),
-      check("from", "From date is Required")
-        .not()
-        .isEmpty(),
       check("skills", "Skills is Required")
         .not()
         .isEmpty()
@@ -145,31 +136,18 @@ router.put(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const {
-      skills,
-      title,
-      location,
-      company,
-      from,
-      to,
-      description
-    } = req.body;
-    const newa = {
-      skills
-    };
-    const newExp = {
-      title,
-      location,
-      company,
-      from,
-      to,
-      description
-    };
+    const { skills } = req.body;
+
+    const newa = {};
+    if (skills) {
+      newa.skills = skills.split(",").map(skill => skill.trim());
+    }
     try {
       const job = await Job.findById(req.params.job_id).populate("students", [
         "name",
         "avatar"
       ]);
+      //Check if student has already applied
       if (
         job.apply.filter(app => app.student.toString() === req.student.id)
           .length > 0
@@ -179,7 +157,54 @@ router.put(
           .json({ msg: "you have already applied this job" });
       }
       job.apply.unshift(newa);
-      //job.apply.experience.unshift(newExp);
+
+      await job.save();
+      res.json(job.apply);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+// route POST api/students/addexperience/:job_id
+// desc view all companies
+// access private
+router.put(
+  "/addexperience/:job_id",
+  [
+    auth,
+    [
+      check("title", "Title is Required")
+        .not()
+        .isEmpty(),
+      check("company", "Company is Required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is Required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    const { title, location, company, from, to, description } = req.body;
+    const newExp = {
+      title,
+      location,
+      company,
+      from,
+      to,
+      description
+    };
+    try {
+      const job = await Job.findById(req.params.job_id);
+      if (!job) {
+        res.status(400).send("No job found");
+      }
+      job.apply.experience.push(newExp);
       await job.save();
       res.json(job.apply);
     } catch (error) {

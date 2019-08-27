4642ba7857247d07f40837614caa8579ad4a7d91
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-08-28 03:45:12 +0500

    Make one route of login to handle to signin student and company

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,12 +10,29 @@ const bcrypt = require("bcrypt");
 // desc Test route
 // access public
 router.get("/", auth, async (req, res) => {
+  const token = req.header("x-auth-token");
   try {
-    const student = await Student.findById(req.student.id).select("-password");
-    res.json(student);
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    if (decoded.student) {
+      const student = await Student.findById(req.student.id).select(
+        "-password"
+      );
+      res.json(student);
+    } else {
+      const company = await Company.findById(req.company.id).select(
+        "-password"
+      );
+      res.json(company);
+    }
   } catch (error) {
     res.status(500).send("Server Error");
   }
+  // try {
+  //   const student = await Student.findById(req.student.id).select("-password");
+  //   res.json(student);
+  // } catch (error) {
+  //   res.status(500).send("Server Error");
+  // }
 });
 
 // route POST api/auth
@@ -34,35 +51,100 @@ router.post(
     }
     const { email, password } = req.body;
     try {
-      // See if student exists
-      let student = await Student.findOne({ email });
-      if (!student) {
+      let check = await Student.findOne({ email });
+      let check1 = await Company.findOne({ email });
+      if (!check && !check1) {
         res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-      // match password
-      const isMatch = await bcrypt.compare(password, student.password);
-      if (!isMatch) {
-        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-      // jsonwebtoken
-      const payload = {
-        student: {
-          id: student.id
+      } else {
+        if (await Student.findOne({ email })) {
+          // See if student exists
+          let student = await Student.findOne({ email });
+          if (!student) {
+            res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+          }
+          // match password
+          const isMatch = await bcrypt.compare(password, student.password);
+          if (!isMatch) {
+            res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+          }
+          // jsonwebtoken
+          const payload = {
+            student: {
+              id: student.id
+            }
+          };
+          jwt.sign(
+            payload,
+            config.get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+        } else if (await Company.findOne({ email })) {
+          // See if company exists
+          let company = await Company.findOne({ email });
+          if (!company) {
+            res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+          }
+          // match password
+          const isMatch = await bcrypt.compare(password, company.password);
+          if (!isMatch) {
+            res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+          }
+          // jsonwebtoken
+          const payload = {
+            company: {
+              id: company.id
+            }
+          };
+          jwt.sign(
+            payload,
+            config.get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
         }
-      };
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      }
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
     }
+
+    // try {
+    // //   // See if student exists
+    // //   let student = await Student.findOne({ email });
+    // //   if (!student) {
+    // //     res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+    // //   }
+    // //   // match password
+    // //   const isMatch = await bcrypt.compare(password, student.password);
+    // //   if (!isMatch) {
+    // //     res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+    // //   }
+    // //   // jsonwebtoken
+    // //   const payload = {
+    // //     student: {
+    // //       id: student.id
+    // //     }
+    // //   };
+    // //   jwt.sign(
+    // //     payload,
+    // //     config.get("jwtSecret"),
+    // //     { expiresIn: 360000 },
+    // //     (err, token) => {
+    // //       if (err) throw err;
+    // //       res.json({ token });
+    // //     }
+    // //   );
+    // // } catch (error) {
+    // //   console.error(error.message);
+    // //   res.status(500).send("Server Error");
+    // // }
   }
 );
 module.exports = router;

ec4d15d0489b7d57001df0729b1b7397d13e9275
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-09-03 19:54:18 +0500

    Get logged in profile

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+  return <div></div>;
+};
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);

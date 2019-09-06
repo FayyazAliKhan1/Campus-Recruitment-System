6ce50b9f05094f4d8fe0b722070e8e6e85731bba
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-09-06 08:47:05 +0500

    Create Spinner and private route component

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import spinner from "../../img/sp.gif";
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      style={{
+        width: "200px",
+        margin: "auto",
+        display: "block"
+      }}
+      alt="loading..."
+    />
+  </Fragment>
+);
+
+export default Spinner;

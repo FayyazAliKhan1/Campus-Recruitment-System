921bfc85a4d42be4c908872a7f6143e2e185b508
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-09-09 10:17:44 +0500

    Render one at a time

--- client/src/components/dashboard/DashboardAction.js
+++ client/src/components/dashboard/DashboardAction.js
@@ -1,19 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const DashboardAction = props => {
-  return (
-    <div class="dash-buttons">
-      <Link to="/edit-profile" class="btn btn-light">
-        <i class="fas fa-user-circle text-primary" /> Edit Profile
-      </Link>
-      <Link to="/add-experience" class="btn btn-light">
-        <i class="fab fa-black-tie text-primary" /> Add Experience
-      </Link>
-      <Link to="/add-education" class="btn btn-light">
-        <i class="fas fa-graduation-cap text-primary" /> Add Education
-      </Link>
-    </div>
-  );
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+const DashboardAction = ({ user }) => (
+  <div>
+    {user.age === undefined ? (
+      <div className="dash-buttons">
+        <Link to="/edit-profilec" className="btn btn-light">
+          <i className="fas fa-user-circle text-primary" /> Edit Profile
+        </Link>
+      </div>
+    ) : (
+      <div className="dash-buttons">
+        <Link to="/edit-profile" className="btn btn-light">
+          <i className="fas fa-user-circle text-primary" /> Edit Profile
+        </Link>
+        <Link to="/add-experience" className="btn btn-light">
+          <i className="fab fa-black-tie text-primary" /> Add Experience
+        </Link>
+        <Link to="/add-education" className="btn btn-light">
+          <i className="fas fa-graduation-cap text-primary" /> Add Education
+        </Link>
+      </div>
+    )}
+  </div>
+);
+DashboardAction.propTypes = {
+  user: PropTypes.object.isRequired
 };
-
-export default DashboardAction;
+const mapStateToProps = state => ({
+  user: state.auth.user
+});
+export default connect(
+  mapStateToProps,
+  {}
+)(DashboardAction);

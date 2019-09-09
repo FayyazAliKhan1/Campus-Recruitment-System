477102087582d309387f2abbef95afa1040c1830
Fayyaz <www.Fayyazali99@yaahoo.com>
2019-09-09 12:43:24 +0500

    Create actions for delete edu and exp and delete account

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  ACCOUNT_DELETED
+} from "./types";
 import { setAlert } from "./alert";
 //Get current user profile
 export const getCurrentProfile = () => async dispatch => {
@@ -135,4 +140,55 @@ export const addEducation = (formData, history) => async dispatch => {
       payload: { msg: error.response.statusText, status: error.response.status }
     });
   }
+};
+//Delete experience
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert("Experience Removed", "success"));
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+//Delete education
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert("Education Removed", "success"));
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+// delete account and profile
+export const deleteAccount = () => async dispatch => {
+  if (window.confirm("Are you sure? you wanna delete"))
+    try {
+      await axios.delete(`/api/profile`);
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+      dispatch(setAlert("Acount Deleted"));
+    } catch (error) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response.statusText,
+          status: error.response.status
+        }
+      });
+    }
 };
--- routes/api/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const auth = require("../../middleware/auth");
-const Job = require("../../models/Jobs");
-const Company = require("../../models/Company");
-
-module.exports = router;
